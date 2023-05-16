ccd345728bdd6baf477aed5f779fe6673c4063ad
Mercedesz A. Feher <mercedesz.a.feher@gmail.com>
2023-05-16 21:25:42 +0200

    fixing img size

--- frontend/raterhood/src/components/Contact.jsx
+++ frontend/raterhood/src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
                 Nimm mit uns Kontakt auf & wir beantworten dir gerne deine Fragen!<br/>
                 Rufe uns an: +43 677 626 56364 oder schreibe uns einfach ein E-Mail: sanja.letich@live.com
             </p>
-            <img src="src/assets/Contact us.jpg"/>
+            <img className="img" src="src/assets/Contact us.jpg"/>
 
             <div>
                 <a href="/">HOME</a>
